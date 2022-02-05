9743ee0ec848659025af7aadc4f8923f3cdd3ead
shetang <sherryeetang@gmail.com>
2022-02-05 17:23:30 -0600

    Practice

--- RStudio-Github Test.R
+++ RStudio-Github Test.R
@@ -3,3 +3,5 @@ print("https://github.com/shetang/testing.git")
 print("This file was created within Rstudio")
 
 print("And now it lives on Github")
+
+pring("https://github.com/shetang/testing.git")
